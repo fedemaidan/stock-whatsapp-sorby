298b547bf5eb4a7eab7df839f25d693a1deb6227
Federico Maidan <fede.maidan@gmail.com>
2025-03-20 13:47:43 -0300

    fix promtp

--- src/Utiles/Chatgpt/Operaciones/ChatModificarConfirmacion.js
+++ src/Utiles/Chatgpt/Operaciones/ChatModificarConfirmacion.js
@@ -7,6 +7,44 @@ const ChatModificarConfirmacion = async (message, userId) => {
     const pedidoAntiguo = FlowManager.userFlows[userId]?.flowData;
     const itemsOriginales = pedidoAntiguo.movimientos;
 
+    const prompt2 = `
+    Como bot de gestión de pedidos de retiro de materiales, debo actualizar el pedido según los cambios solicitados por el usuario, sin sobreescribir completamente el pedido anterior. Para ello, debo interpretar la solicitud y aplicar una de las siguientes acciones:
+
+Aprobados: Todo lo que no fue rechazado debe ser aprobado. Es decir, si tengo X de un producto, y el cliente dice que falta Y del mismo producto. Tengo aprobado X-Y
+
+Rechazados: Si una parte del pedido no puede ser aprobada, la cantidad rechazada debe reflejarse en la lista de productos rechazados.
+
+Modificar cantidad: Si el usuario pide cambiar la cantidad de un producto, actualizar la cantidad a la solicitada.
+
+Estructura esperada del JSON de respuesta:
+
+"aprobados" debe contener solo las cantidades aprobadas del pedido.
+"rechazados" debe contener las cantidades no aprobadas.
+Las suma de la cantidad del aprobados y rechazados deben ser igual a la de items_originales
+\`\`\`
+{
+  "items_originales": ${JSON.stringify(itemsOriginales, null, 2)},
+  "aprobados": [
+    {
+      "producto_name": "nombre del producto",
+      "cantidad": "cantidad del producto",
+      "obra_origen": "obra que sedio los productos",
+      "obra_destino": "obra a la que se destinaron (puede estar vacio)"
+    }
+  ],
+  "rechazados": [
+    {
+      "producto_name": "nombre del producto",
+      "cantidad": "cantidad del producto",
+      "obra_origen": "obra que sedio los productos",
+      "obra_destino": "obra a la que se destinaron (puede estar vacio)"
+    }
+  ]
+}
+\`\`\`
+Mensaje del cliente: "${message}"`
+
+
     const prompt = `
 Como bot de gestión de pedidos de retiro de materiales, debo actualizar el pedido según los cambios solicitados por el usuario, sin sobreescribir completamente el pedido anterior. Para ello, debo interpretar la solicitud y aplicar una de las siguientes acciones:
 
