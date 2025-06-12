8ad8fd9bf2d9951f0abc9e9f04acb286c9d45e4b
Alexis <alexis.tumino@gmail.com>
2025-06-12 10:31:37 -0300

    api 1

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -9,6 +9,10 @@ const QRCode = require('qrcode');
 const express = require('express');
 const app = express();
 const SocketSingleton = require('../src/Utiles/SockSingleton');
+const materiales = require('./materiales.routes')
+const cors = require('cors');
+
+app.use(cors());
 
 // Variable para almacenar el último QR generado (si se requiere)
 let latestQR = null;
