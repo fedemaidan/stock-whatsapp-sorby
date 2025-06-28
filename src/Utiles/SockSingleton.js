cd24848c328adac74f0de48425b8cf4e098945bd
Alexis <alexis.tumino@gmail.com>
2025-06-28 11:33:51 -0300

    Integracion de libreria version 2.1.3 para stock

--- src/Utiles/SockSingleton.js
+++ src/Utiles/SockSingleton.js
@@ -11,30 +11,43 @@ class SockSingleton {
     }
     async setSock(sockInstance) {
         this.sock = sockInstance;
-        autoReporter.startAutoReport(this.sock,"sorby_stock","http://localhost:4000/api/reportar");
+        autoReporter.startAutoReport(this.sock, "sorby_stock", "http://localhost:4000/api/reportar");
         this.sock.ev.on('messages.upsert', async (message) => {
-            
+
             if (message.type === 'notify') {
                 const msg = message.messages[0];
+
+                if (msg.key.fromMe) {
+                    if (
+                        msg.message?.conversation === 'TODO_OK' ||
+                        msg.message?.extendedTextMessage?.text === 'TODO_OK'
+                    ) {
+                        console.log("🟢 Mensaje TODO_OK recibido, marcando ping como OK.");
+                        autoReporter.marcarPingOK();
+                    }
+
+                    return; // ← cortamos toda respuesta automática a fromMe
+                }
+
                 if (!msg.message || msg.key.fromMe) return;
 
                 const sender = msg.key.remoteJid;
                 const messageType = GetMessageType(msg.message);
 
-                await messageResponder(messageType,msg, this.sock ,sender);
+                await messageResponder(messageType, msg, this.sock, sender);
             }
-            else if (message.type === 'append') {}
+            else if (message.type === 'append') { }
         });
         setInterval(async () => await this.sock.sendPresenceUpdate('available'), 10 * 60 * 1000);
     }
     // Obtiene la instancia del sock
     getSock() {
-    if (!this.sock) {
-        console.error('🛑 Sock aún no está listo, espera antes de enviar el mensaje.');
-        return null;
+        if (!this.sock) {
+            console.error('🛑 Sock aún no está listo, espera antes de enviar el mensaje.');
+            return null;
+        }
+        return this.sock;
     }
-    return this.sock;
-}
 
 }
 module.exports = new SockSingleton();
